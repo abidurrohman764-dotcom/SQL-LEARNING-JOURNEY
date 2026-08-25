63688599b98511e70a45e17cd64e84a956f3e939
abdrhmn <abidurrohman764@gmail.com>
2026-08-25 11:36:11 +0700

    Create Practice SELECT & FROM.sql

---
+++ SQL Dasar Minggu 1-2/Practice SELECT & FROM.sql
@@ -0,0 +1,24 @@
+Soal 1
+Manager ingin melihat semua data.
+JAWABAN
+SELECT * 
+FROM sales;
+
+Soal 2
+Manager hanya ingin melihat product dan quantity
+JAWABAN
+SELECT product, quantity
+FROM sales;
+
+Soal 3
+Manager ingin melihat product category price
+JAWABAN
+SELECT product, category, price
+FROM sales;
+
+Soal 4
+Manager berkata "Saya ingin melihat ID transaksi, produk, dan harga."
+Kolom apa saja yang harus kamu ambil?
+JAWABAN
+SELECT transaction_id, product, price
+FROM sales;
