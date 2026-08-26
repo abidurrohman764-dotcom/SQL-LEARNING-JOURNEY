2ad925baf09eebb8cd0d8c75c4fd42e7776df510
abdrhmn <abidurrohman764@gmail.com>
2026-08-26 19:35:08 +0700

    Create Materi Operator Perbandingan.sql

---
+++ SQL Dasar Minggu 1-2/Materi Operator Perbandingan.sql
@@ -0,0 +1,28 @@
+Operator	Arti
+=	sama dengan
+>	lebih besar
+<	lebih kecil
+>=	lebih besar atau sama dengan
+<=	lebih kecil atau sama dengan
+<>	tidak sama dengan
+
+Contoh:
+Lebih besar dari
+WHERE quantity > 10
+  
+Lebih kecil dari
+WHERE quantity < 10
+  
+Lebih besar atau sama dengan
+WHERE quantity >= 10
+Artinya:
+quantity 10 ikut masuk.
+
+Lebih kecil atau sama dengan
+WHERE quantity <= 10
+  
+Tidak sama dengan
+WHERE category <> 'Sembako'
+
+Artinya:
+Ambil data yang kategorinya bukan Sembako.
