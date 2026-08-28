2c351c228880d1a72daafe4f8566e2622d2a7700
abdrhmn <abidurrohman764@gmail.com>
2026-08-28 18:47:54 +0700

    Update Practice Aggregate Fuction.sql

--- SQL Dasar Minggu 1-2/Practice Aggregate Fuction.sql
+++ SQL Dasar Minggu 1-2/Practice Aggregate Fuction.sql
@@ -44,3 +44,46 @@ AVG(quantity),
 MAX(price),
 MIN(price)
 FROM sales;
+
+----------------------------------------------
+
+Soal 1 Aggregate + AS
+Hitung jumlah seluruh transaksi dan beri nama total_transactions
+SELECT COUNT(*) AS total_transactions
+FROM sales;
+
+Soal 2 Aggregate + AS
+Hitung total quantity dan beri nama total_quantity
+SELECT SUM(quantity) AS total_quantity
+FROM sales;
+
+Soal 3 Aggregate + AS
+Hitung rata-rata harga dan beri nama average_price
+SELECT AVG(price) AS average_price
+FROM sales;
+
+Soal 4 Aggregate + AS
+Tampilkan:
+harga tertinggi → highest_price
+harga terendah → lowest_price
+Dalam satu query.
+SELECT MAX(price) AS highest_price,
+MIN(price) AS lowest_price
+FROM sales;
+
+Soal 5 Aggregate + AS
+Buat satu query yang menghasilkan:
+
+KPI	Nama Alias
+Jumlah transaksi	total_transactions
+Total barang	total_quantity
+Rata-rata quantity	average_quantity
+Harga tertinggi	highest_price
+Harga terendah	lowest_price
+
+SELECT COUNT(*) AS total_transactions, 
+SUM(quantity) AS total_quantity,
+AVG(quantity) AS average_quantity,
+MAX(price) AS highest_price,
+MIN(price) AS lowest_price
+FROM sales;
